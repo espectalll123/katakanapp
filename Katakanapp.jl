e81d5e32f9d7d9ff13dd19eb07e301fdae00c526
Francisco Gómez <espectalll123@kydara.com>
2016-03-30 12:51:48 +0200

    Now the app does things

--- Katakanapp.jl
+++ Katakanapp.jl
@@ -9,43 +9,83 @@ input = GAccessor.object(bld, "userInput")
 kanaLbl = GAccessor.object(bld, "kanaLabel")
 answerLbl = GAccessor.object(bld, "answerLabel")
 
-dic = [["ア" "エ" "ウ" "エ"　"オ"
-	"カ" "キ" "ク" "ケ"　"コ"
-	"サ" "シ" "ス" "セ" "ソ"
-	"タ" "チ" "ツ" "テ" "ト"
-	"ナ" "ニ" "ヌ" "ネ" "ノ"
-	"ハ" "ヒ" "フ" "ヘ" "ホ"
-	"マ" "ミ" "ム" "メ" "モ"
-	"ヤ" ( ) "ユ" ( ) "ヨ" 
-	"ラ" "リ" "ル" "レ" "ロ"
-	"ワ" "ヰ" ( ) "ヱ" "ヲ"];
-	["a" "i" "u" "e"　"o"
-	"ka" "ki" "ku" "ke"　"ko"
-	"sa" "shi" "su" "se" "so"
-	"ta" "ti" "tsu" "te" "to"
-	"na" "ni" "nu" "ne" "no"
-	"ha" "hi" "hu" "he" "ho"
-	"ma" "mi" "mu" "me" "mo"
-	"ya" ( ) "yu" ( ) "yo" 
-	"ra" "ri" "ru" "re" "ro"
-	"wa" "wi" ( ) "we" "wo"]]
+dic = Vector{AbstractString}[
+    ["ア", "エ", "ウ", "エ",　"オ",
+	"カ", "キ", "ク", "ケ",　"コ",
+	"キャ", "キュ",　"キョ",
+	"サ", "シ", "ス", "セ", "ソ",
+	"シャ", "シュ", "ショ",
+	"タ", "チ", "ツ", "テ", "ト",
+	"チャ", "チュ", "チョ",
+	"ナ", "ニ", "ヌ", "ネ", "ノ",
+	"ニャ", "ニュ", "ニョ",
+	"ハ", "ヒ", "フ", "ヘ", "ホ",
+	"ヒャ", "ヒュ", "ヒョ",
+	"マ", "ミ", "ム", "メ", "モ",
+	"ミャ", "ミュ", "ミョ",
+	"ヤ",       "ユ",       "ヨ",
+	"ラ", "リ", "ル", "レ", "ロ",
+	"リャ", "リュ", "リュ",
+	"ワ", "ヰ",       "ヱ", "ヲ",
+	"ガ", "ギ", "グ", "ゲ", "ゴ",
+	"ギャ", "ギュ", "ギョ",
+	"ザ", "ジ", "ズ", "ゼ", "ゾ",
+	"ジャ", "ジュ", "ジョ",
+	"ダ", "ヂ", "ヅ", "デ", "ド",
+	"ヂャ", "ヂュ", "ヂョ",
+	"バ", "ビ", "ブ", "ベ", "ボ",
+	"ビャ", "ビュ", "ビョ",
+	"パ", "ピ", "プ", "ペ", "ポ",
+	"ピャ", "ピュ", "ピョ"],
+	["a", "i", "u", "e",　"o",
+	"ka", "ki", "ku", "ke",　"ko",
+	"kya", "kyo", "kyu",
+	"sa", "shi", "su", "se", "so",
+	"sha", "sho", "shu",
+	"ta", "chi", "tsu", "te", "to",
+	"cha", "cho", "chu",
+	"na", "ni", "nu", "ne", "no",
+	"nya", "nyo", "nyu",
+	"ha", "hi", "hu", "he", "ho",
+	"hya", "hyo", "hyu",
+	"ma", "mi", "mu", "me", "mo",
+	"mya", "myu", "myo",
+	"ya",       "yu",       "yo",
+	"ra", "ri", "ru", "re", "ro",
+	"rya", "ryu", "ryo",
+	"wa", "wi",       "we", "wo",
+	"ga", "gi", "gu", "ge", "go",
+	"gya", "gyu", "gyo",
+	"za", "ji", "zu", "ze", "zo",
+	"ja", "ju", "jo",
+	"da", "(d)ji", "(d)zu", "de", "do",
+	"(d)ja", "(d)ju", "(d)jo",
+	"ba", "bi", "bu", "be", "bo",
+	"bya", "byu", "byo",
+	"pa", "pi", "pu", "pe", "po",
+	"pya", "pyu", "pyo"]]
 
-function pickUpKanaId()
-    return round(Int, rand() * 51)
+function pickUpKana()
+    rndId = round(Int, rand() * length(dic[1]))
+    return (dic[1][rndId], dic[2][rndId])
 end
 
 signal_connect(sendBtn, :clicked) do widget
+    global kana
 	idata = getproperty(input, :text, AbstractString)
-	setproperty!(kanaLbl, :label, "Text A");
-    setproperty!(answerLbl, :label, "Text B");
+	if idata == kana[2]
+	    kana = pickUpKana()
+	    setproperty!(kanaLbl, :label, kana[1])
+        setproperty!(answerLbl, :label, kana[2])
+    end
 end
 signal_connect(win, :destroy) do widget
 	Gtk.gtk_quit()
 end
 
-rndId = pickUpKanaId()
-setproperty!(kanaLbl, :label, dic[rndId]);
-setproperty!(answerLbl, :label, dic[rndId]);
+kana = pickUpKana()
+setproperty!(kanaLbl, :label, kana[1])
+setproperty!(answerLbl, :label, kana[2])
 
 showall(win)
 Gtk.gtk_main()
